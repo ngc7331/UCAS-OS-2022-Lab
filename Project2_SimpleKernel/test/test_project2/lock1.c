f57c49e6c470d0e417cd0ca689f2316660b81691
xuzhihao <xuzhihao20@mails.ucas.ac.cn>
2022-10-11 11:21:08 +0800

    feat(p2-task4): remove sys_yield() in apps

--- Project2_SimpleKernel/test/test_project2/lock1.c
+++ Project2_SimpleKernel/test/test_project2/lock1.c
@@ -66,7 +66,7 @@ int main(void)
         sys_move_cursor(0, print_location);
         printf("> [TASK] Applying for a lock.\n");
 
-        sys_yield();
+        // sys_yield();
 
         sys_mutex_acquire(mutex_id);
 
@@ -74,7 +74,7 @@ int main(void)
         {
             sys_move_cursor(0, print_location);
             printf("> [TASK] Has acquired lock and running.(%d)\n", i);
-            sys_yield();
+            // sys_yield();
         }
 
         sys_move_cursor(0, print_location);
@@ -85,7 +85,7 @@ int main(void)
 
         sys_mutex_release(mutex_id);
 
-        sys_yield();
+        // sys_yield();
     }
 
     return 0;
