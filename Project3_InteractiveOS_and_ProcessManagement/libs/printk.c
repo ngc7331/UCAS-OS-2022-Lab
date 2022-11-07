3fd4efa43d77edeb9253e6b68e4f2bbba401502b
xu_zh <xuzhihao20@mails.ucas.ac.cn>
2022-11-07 17:18:00 +0800

    fix(p3): logging: increase bufsize

--- Project3_InteractiveOS_and_ProcessManagement/libs/printk.c
+++ Project3_InteractiveOS_and_ProcessManagement/libs/printk.c
@@ -305,7 +305,7 @@ int logging(loglevel_t level, const char* name, const char *fmt, ...)
     return -1;
 #else
     if (level < __level) return -1;
-    char buf[40];
+    char buf[42];
     char __dict[7] = "VDIWEC";
     int ret;
     va_list va;
