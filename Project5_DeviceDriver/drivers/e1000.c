2a6a4dcbe7a84f236ac5728a74bcdd4f4cb8f617
xu_zh <xuzhihao20@mails.ucas.ac.cn>
2022-12-01 17:23:42 +0800

    feat(p5-task1): send

--- Project5_DeviceDriver/drivers/e1000.c
+++ Project5_DeviceDriver/drivers/e1000.c
@@ -4,6 +4,7 @@
 #include <os/time.h>
 #include <assert.h>
 #include <pgtable.h>
+#include <printk.h>
 
 // E1000 Registers Base Pointer
 volatile uint8_t *e1000;  // use virtual memory address
@@ -54,13 +55,29 @@ static void e1000_reset(void)
  **/
 static void e1000_configure_tx(void)
 {
-    /* TODO: [p5-task1] Initialize tx descriptors */
+    /* Initialize tx descriptors */
+    for (int i=0; i<TXDESCS; i++) {
+        tx_desc_array[i].addr = 0;
+        tx_desc_array[i].length = 0;
+        tx_desc_array[i].cso = 0;
+        tx_desc_array[i].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+        tx_desc_array[i].status = 0;
+        tx_desc_array[i].css = 0;
+        tx_desc_array[i].special = 0;
+    }
 
-    /* TODO: [p5-task1] Set up the Tx descriptor base address and length */
+    /* Set up the Tx descriptor base address and length */
+    e1000_write_reg(e1000, E1000_TDBAL, kva2pa((uint64_t) tx_desc_array) & 0xffffffff);
+    e1000_write_reg(e1000, E1000_TDBAH, kva2pa((uint64_t) tx_desc_array) >> 32);
+    e1000_write_reg(e1000, E1000_TDLEN, sizeof(tx_desc_array));
 
-	/* TODO: [p5-task1] Set up the HW Tx Head and Tail descriptor pointers */
+	/* Set up the HW Tx Head and Tail descriptor pointers */
+    e1000_write_reg(e1000, E1000_TDH, 0);
+    e1000_write_reg(e1000, E1000_TDT, 0);
 
-    /* TODO: [p5-task1] Program the Transmit Control Register */
+    /* Program the Transmit Control Register */
+    e1000_write_reg(e1000, E1000_TCTL, E1000_TCTL_EN | E1000_TCTL_PSP |
+                                                      (E1000_TCTL_CT & (0x10 << 4)) | (E1000_TCTL_COLD & (0x40 << 12)));
 }
 
 /**
@@ -104,9 +121,26 @@ void e1000_init(void)
  **/
 int e1000_transmit(void *txpacket, int length)
 {
-    /* TODO: [p5-task1] Transmit one packet from txpacket */
+    /* Transmit one packet from txpacket */
+    uint32_t tdh, tdt;
+    do {
+        tdh = e1000_read_reg(e1000, E1000_TDH);
+        tdt = e1000_read_reg(e1000, E1000_TDT);
+    } while ((tdt+1) % TXDESCS == tdh);  // full
+    logging(LOG_DEBUG, "e1000", "... tdt=%u, tdh=%u\n", tdt, tdh);
 
-    return 0;
+    // copy to buf
+    memcpy((uint8_t *) &tx_pkt_buffer[tdt], txpacket, length);
+    logging(LOG_DEBUG, "e1000", "... buf=%lx\n", (uint64_t) &tx_pkt_buffer[tdt]);
+
+    // write desp
+    tx_desc_array[tdt].addr = kva2pa((uint64_t) tx_pkt_buffer[tdt]);
+    tx_desc_array[tdt].length = length;
+
+    // set tdt
+    e1000_write_reg(e1000, E1000_TDT, (tdt+1) % TXDESCS);
+
+    return length;
 }
 
 /**
--- Project5_DeviceDriver/kernel/net/net.c
+++ Project5_DeviceDriver/kernel/net/net.c
@@ -4,21 +4,25 @@
 #include <os/string.h>
 #include <os/list.h>
 #include <os/smp.h>
+#include <printk.h>
 
 static LIST_HEAD(send_block_queue);
 static LIST_HEAD(recv_block_queue);
 
-int do_net_send(void *txpacket, int length)
-{
-    // TODO: [p5-task1] Transmit one network packet via e1000 device
+int do_net_send(void *txpacket, int length) {
+    int cid = get_current_cpu_id();
+    logging(LOG_INFO, "e1000", "%d.%s.%d send from %lx, length = %d\n",
+            current_running[cid]->pid, current_running[cid]->name, current_running[cid]->tid, (uint64_t) txpacket, length);
+    // Transmit one network packet via e1000 device
+    e1000_transmit(txpacket, length);
+
     // TODO: [p5-task3] Call do_block when e1000 transmit queue is full
     // TODO: [p5-task4] Enable TXQE interrupt if transmit queue is full
 
-    return 0;  // Bytes it has transmitted
+    return length;  // Bytes it has transmitted
 }
 
-int do_net_recv(void *rxbuffer, int pkt_num, int *pkt_lens)
-{
+int do_net_recv(void *rxbuffer, int pkt_num, int *pkt_lens) {
     // TODO: [p5-task2] Receive one network packet via e1000 device
     // TODO: [p5-task3] Call do_block when there is no packet on the way
 
