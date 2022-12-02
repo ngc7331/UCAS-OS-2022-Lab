1fd13644dc4bfcde59037410104ddcb734648905
xu_zh <xuzhihao20@mails.ucas.ac.cn>
2022-12-03 02:35:08 +0800

    Revert "feat(p5-task5): echo: use rounded buffer"
    This reverts commit 473fe622b1b1bec8ef379032d0320d22a601aa63.

--- Project5_DeviceDriver/test/test_project5/echo.c
+++ Project5_DeviceDriver/test/test_project5/echo.c
@@ -7,13 +7,10 @@
 #define MUTEX_KEY 114514 
 #define COND_KEY_FULL 1919
 #define COND_KEY_EMPTY 810
-#define BUFNUM 8
-#define BUFSIZE 256
 
 typedef struct {
-    int rp, sp;
-    int length[BUFNUM];
-    char buf[BUFNUM][BUFSIZE];
+    int length;
+    uint8_t buf[2048];
 } shm_buf_t;
 
 void clearline(int line) {
@@ -27,8 +24,8 @@ void recv() {
     printf("[recv] loaded, init ...");
     shm_buf_t *shm = (shm_buf_t *) sys_shmpageget(SHM_KEY);
     int mutex = sys_mutex_init(MUTEX_KEY);
-    int not_empty = sys_condition_init(COND_KEY_FULL);
-    int not_full = sys_condition_init(COND_KEY_EMPTY);
+    int full = sys_condition_init(COND_KEY_FULL);
+    int empty = sys_condition_init(COND_KEY_EMPTY);
     int round = 0;
     printf("done\n");
 
@@ -36,25 +33,22 @@ void recv() {
     printf("[recv] #");
     while (1) {
         clearline(1);
-        printf("%d: rp=%d, process=", round, shm->rp);
+        printf("%d: ", round);
         sys_mutex_acquire(mutex);
 
-        // wait until buffer is not full
-        while ((shm->sp+1) % BUFNUM == shm->rp) {
-            printf("W");
-            sys_condition_wait(not_full, mutex);
-        }
+        // wait until buffer is empty
+        printf("waiting ... ");
+        while (shm->length != 0)
+            sys_condition_wait(empty, mutex);
 
         // recv from net
-        printf("R");
+        printf("receiving ... ");
         int lens[1];
-        int rp = shm->rp;
-        shm->length[rp] = sys_net_recv(&shm->buf[rp], 1, lens);
-        shm->rp = (rp + 1) % BUFNUM;
+        shm->length = sys_net_recv(&shm->buf, 1, lens);
 
         // wake up sender
-        printf("D\n");
-        sys_condition_signal(not_empty);
+        printf("done\n");
+        sys_condition_signal(full);
 
         sys_mutex_release(mutex);
         round ++;
@@ -70,8 +64,8 @@ void send() {
     printf("[send] loaded, init ...");
     shm_buf_t *shm = (shm_buf_t *) sys_shmpageget(SHM_KEY);
     int mutex = sys_mutex_init(MUTEX_KEY);
-    int not_empty = sys_condition_init(COND_KEY_FULL);
-    int not_full = sys_condition_init(COND_KEY_EMPTY);
+    int full = sys_condition_init(COND_KEY_FULL);
+    int empty = sys_condition_init(COND_KEY_EMPTY);
     int round = 0;
     printf("done\n");
 
@@ -79,45 +73,43 @@ void send() {
     printf("[send] #");
     while (1) {
         clearline(2);
-        printf("%d: sp=%d, process=", round, shm->sp);
+        printf("%d: ", round);
         sys_mutex_acquire(mutex);
 
-        // wait until buffer is not empty
-        while (shm->sp == shm->rp) {
-            printf("W");
-            sys_condition_wait(not_empty, mutex);
-        }
+        // wait until buffer is empty
+        printf("waiting ... ");
+        while (shm->length == 0)
+            sys_condition_wait(full, mutex);
 
         // recv from net
-        printf("S");
-        int sp = shm->sp;
+        printf("sending ... ");
         // replace target MAC to broadcast
-        shm->buf[sp][0] = 0xff;
-        shm->buf[sp][1] = 0xff;
-        shm->buf[sp][2] = 0xff;
-        shm->buf[sp][3] = 0xff;
-        shm->buf[sp][4] = 0xff;
-        shm->buf[sp][5] = 0xff;
+        shm->buf[0] = 0xff;
+        shm->buf[1] = 0xff;
+        shm->buf[2] = 0xff;
+        shm->buf[3] = 0xff;
+        shm->buf[4] = 0xff;
+        shm->buf[5] = 0xff;
         // replace "Requests:" to "Response:"
-        for (int i=6; i<shm->length[sp]; i++) {
-            if (strncmp("Requests:", shm->buf[sp]+i, 8) == 0) {
-                shm->buf[sp][i] = 'R';
-                shm->buf[sp][i+1] = 'e';
-                shm->buf[sp][i+2] = 's';
-                shm->buf[sp][i+3] = 'p';
-                shm->buf[sp][i+4] = 'o';
-                shm->buf[sp][i+5] = 'n';
-                shm->buf[sp][i+6] = 's';
-                shm->buf[sp][i+7] = 'e';
+        for (int i=6; i<shm->length; i++) {
+            if (strncmp("Requests:", shm->buf+i, 8) == 0) {
+                shm->buf[i] = 'R';
+                shm->buf[i+1] = 'e';
+                shm->buf[i+2] = 's';
+                shm->buf[i+3] = 'p';
+                shm->buf[i+4] = 'o';
+                shm->buf[i+5] = 'n';
+                shm->buf[i+6] = 's';
+                shm->buf[i+7] = 'e';
                 break ;
             }
         }
-        sys_net_send(&shm->buf[sp], shm->length[sp]);
-        shm->sp = (sp + 1) % BUFNUM;
+        sys_net_send(&shm->buf, shm->length);
+        shm->length = 0;
 
         // wake up sender
-        printf("D\n");
-        sys_condition_signal(not_full);
+        printf("done\n");
+        sys_condition_signal(empty);
 
         sys_mutex_release(mutex);
         round ++;
